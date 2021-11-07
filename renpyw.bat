a7b126b2a0f07dac048fce9766a99856da6d8223
Proudust <proudust@gmail.com>
2021-11-07 13:06:10 +0900

    :sparkles: Batch always uses Python included with Ren'Py

--- renpyw.bat
+++ renpyw.bat
@@ -1,33 +1,25 @@
 @echo off
 
-set try_download=0
-goto :find_by_renpy
+rem Version of the Ren'Py SDK to be installed
+set renpy_version=6.99.12.4
 
-:find_by_path
-where $path:python /q
-if errorlevel 1 (
-    python ./build
-    goto :EOF
-) else (
-    python ./build
-    goto :EOF
-)
+set try_download=0
 
 :find_by_renpy
 if exist "lib\renpy\lib\windows-i686\python.exe" (
-    .\lib\renpy\lib\windows-i686\python.exe -O ./build
+    .\lib\renpy\lib\windows-i686\python.exe -O ./build.py
     goto :EOF
 )
 
 :download_renpy_sdk
 if %try_download% == 1 goto :error
 set try_download = 1
-bitsadmin /TRANSFER RENPY https://www.renpy.org/dl/6.99.12.4/renpy-6.99.12.4-sdk.7z.exe %~dp0\renpy.7z.exe
+bitsadmin /TRANSFER RENPY https://www.renpy.org/dl/%renpy_version%/renpy-%renpy_version%-sdk.7z.exe %~dp0\renpy.7z.exe
 if errorlevel 1 goto :error
 start /wait renpy.7z.exe x -y -olib
 del renpy.7z.exe
 timeout /T 1
-rename lib\renpy-6.99.12.4-sdk renpy
+rename lib\renpy-%renpy_version%-sdk renpy
 goto :find_by_renpy
 
 :error
